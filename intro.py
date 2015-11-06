d681ad928860d8634888fcdee711e91eaf90134f
Mikolaj Buchwald <mikolaj.buchwald@gmail.com>
2015-11-06 19:43:53 +0100

    intro.py init commit

---
+++ intro.py
@@ -0,0 +1,72 @@
+# Introductory script. The most important python expressions and operations.
+
+############################################
+#                                          #
+#              VARIABLES                   #
+#                                          #
+############################################
+
+################################
+#           variable           #
+################################
+a = 5
+
+################################
+#      types of variables      #
+################################
+
+# string variable
+b = "t"
+c = "cognitive computing"
+
+# float variable
+d = 25.8
+
+# boolean variable
+e = True
+
+# type() function
+type(a)
+# out: int
+type(b)
+# out: string
+type(c)
+# out: string
+type(d)
+# out: float
+type(e)
+# out: bool
+
+################################
+#    variables constructors    #
+################################
+
+# int(), float(), str()
+int('37')
+# out: 37
+type('37')
+# out: str
+type(int('37'))
+# out: int
+
+float(2)
+# out: 2.0
+float(a)
+# out: 5.0
+
+str(0.12)
+# out: '0.12'
+
+
+############################################
+#                                          #
+#              CALCULATOR                  #
+#                                          #
+############################################
+
+
+############################################
+#                                          #
+#                 LISTS                    #
+#                                          #
+############################################
